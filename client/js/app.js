1114c37010abddfc3790c337e2a64866c96f0bf6
Glen E. Ivey <gei@mcn.org>
2012-02-17 23:27:32 -0800

    remove redundant HTML markup; generate from data in JS

--- client/js/app.js
+++ client/js/app.js
@@ -12,6 +12,48 @@ socket.on('drop', function (data) {
 });
 
 $(function() {
+  var html = '';
+  var data = [
+    [ "mark",      "Mark",      "mm",  "D"  ],
+    [ "nick",      "Nick",      "nrs", "D"  ],
+    [ "glen",      "Glen",      "gi",  null ],
+    [ "lewis",     "Lewis",     "lh",  "D"  ],
+    [ "chad",      "Chad",      "caw", null ],
+    [ "alex",      "Alex",      "aj",  "D"  ],
+    [ "matt",      "Matt",      "mh",  "D"  ],
+    [ "thomas",    "Thomas",    "tb",  "SF" ],
+    [ "david",     "David",     "ds",  "SF" ],
+    [ "jordi",     "Jordi",     "jn",  "SF" ],
+    [ "dan",       "Dan",       "dp",  null ],
+    [ "chris",     "Chris",     "ct",  null ],
+    [ "jo",        "Jo",        "jw",  null ],
+    [ "christian", "Christian", "cn",  "SF" ],
+    [ "johan",     "Johan",     "ji",  "SF" ]
+  ];
+
+
+  html += '<table>';
+  var pairs = (data.length / 2) + 2;
+  for (var i=0; i < pairs; i++) {
+    html += '<tr><td id="pA' + i +'">&nbsp;</td>';
+    html +=     '<td id="pB' + i +'">&nbsp;</td></tr>';
+  }
+  html += '</table>\n';
+
+  for (var i=0; i < data.length; i++) {
+    var id=data[i][0], name=data[i][1], location=data[i][3];
+    html += '<div id="' + id + '" class="person">';
+
+    if (location) {
+      html += '<div class="where">' + location + '</div>';
+    }
+
+    html += name + '</div>';
+  }
+
+  $('body').html(html);
+
+
   var x = 10;
   var y = 80;
   $('.person').draggable().each(function() {
