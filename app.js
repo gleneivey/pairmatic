af00b3f73d956cb7711ec1f0ace3e5b6f80540b1
Glen Ivey <glenivey@pivotallabs.com>
2012-02-17 19:42:19 -0800

    initial commit; toy version working

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(80);
+
+function handler (req, res) {
+  var url = req.url;
+  if (url == '/') { url = '/index.html'; }
+  fs.readFile(__dirname + url,
+      function (err, data) {
+	if (err) {
+	  res.writeHead(500);
+	  return res.end('Error loading "' + url + '"');
+	}
+
+	res.writeHead(200);
+	res.end(data);
+      }
+  );
+}
+
+
+io.sockets.on('connection', function (socket) {
+    socket.on('drop', function (data) {
+        socket.broadcast.emit('drop', data);
+    });
+});
