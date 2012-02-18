2aea358b91b2577c3c1a0caf8d89476b1b5a2988
Glen E. Ivey <gei@mcn.org>
2012-02-18 14:10:46 -0800

    missed copy of GFDL, file headers when adding copyright stuff

--- server/app.js
+++ server/app.js
@@ -1,3 +1,21 @@
+// PairMatic - a real-time remote pair coordinating web app
+// Copyright (C) 2012 - Glen E. Ivey
+// https://github.com/gleneivey/pairmatic
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version
+// 3 as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program in the file COPYING and/or LICENSE. If not,
+// see <http://www.gnu.org/licenses/>.
+
+
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
 var fs = require('fs');
